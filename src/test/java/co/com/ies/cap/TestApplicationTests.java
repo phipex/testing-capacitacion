5a723b799628a30f7e41ff9fa6534971f50a8729
Andres Montoya @phipex <aphipex@yahoo.com>
2021-02-08 13:23:34 -0500

    chore: configuracion inicial

---
+++ src/test/java/co/com/ies/cap/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ies.cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
